20cf4d93c5f126d9399e9c2002382e2964e2fcdf
Ricardo Flores <richie.flores.pumas@gmail.com>
2019-10-24 22:38:36 -0500

    Tarea4

--- TAREAS/4/main.c
+++ TAREAS/4/main.c
@@ -3,11 +3,14 @@
 float m;
 float y=0;
 float promedio;
+//Declarando las variables
 int main(int argc, char const *argv[]){
+	//construyendo la repetición con for
 	for (int i = 0; i < argc; ++i){
 		m=atof(argv[i]);
 		y=y+m;
 	}
+	//Esta es la opreacion para generar el promedio
 	promedio=y/(argc-1);
 	printf("%.2lf\n",promedio );
 	return 0;
