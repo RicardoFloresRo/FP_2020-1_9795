9e16851d0f581ef03063f7426e5bba411651e3c2
Ricardo Flores <richie.flores.pumas@gmail.com>
2019-11-07 19:08:08 -0600

    Tarea 7 corrección

--- TAREAS/7/main.c
+++ TAREAS/7/main.c
@@ -2,11 +2,21 @@
 #include <stdlib.h>
 #include <math.h>
 
-int main(int argc, char *argv[]) {
-//nos ayuda a tomar valores que se enecesitan,que es la base y a que exponente se quiere aumentar
-int b= atoi(argv[1]); //base
-int e= atoi(argv[2]);//exponente
-int p= pow(b,e);//esta funcion  es la que realiza la operacion
-printf("%i\n",p);
+int main(int argc, char *argu[]) {
+//declaramos variables
+	int a;
+	int b;
+	int c=1;
+	a=atof(argu[1]);
+	b=atof(argu[2]);
+	//Ahora vemos donde la vamos a almacenar
+	for (int i = 0; i < b; i++)
+	{
+		//decimos en donde se va a guardar y que se va a multiplicar
+		c=c*a
+	}
+	//imprimimos el resultado
+	printf("%.2d\n",c );
+
 return 0;
 }
