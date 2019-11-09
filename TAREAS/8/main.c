e978624356d5a28049cc812fe057d7c25e44f413
Ricardo Flores <richie.flores.pumas@gmail.com>
2019-11-08 23:56:44 -0600

    Tarea 8

--- TAREAS/8/main.c
+++ TAREAS/8/main.c
@@ -3,19 +3,20 @@
 #include <math.h>
 
 int main(int argc,char *argv[]){
-//la x es lo que queremos sacar
-//el valor a convertir es y
-int x= atoi(argv[1]);
-int y= atoi(argv[2]);
-if(x!=0){
-//este pasa de fahrenhait a celsius
-float p= (y-32)/1.8;
-printf("%.2lf",p);
-}
-else{
-//este  de celsius a fahrenhait
-float f= (y*1.8)+32;
-printf("%.2lf",f);
+    //la x es lo que queremos sacar
+    //el valor a convertir es y
+    float x= atof(argv[1]);
+    float y= atof(argv[2]);
+    if(x!=0){ 
+    //este pasa de fahrenhait a celsius
+        float p= (y-32)/1.8;
+        printf("% .2lf",p);
+    }
+    else{
+    //este  de celsius a fahrenhait
+        float f= (y*1.8)+32;
+        printf("% .2lf",f);
+    }
+    return 0;
+}  
 
-}
-}
